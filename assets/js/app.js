89172b7e3e24b31542b4d7b18a49623c98421c79
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-10-10 21:25:38 -0500

    was able to fix functionality with the AJAX call

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 var movies = [
     "Edward Scissorhands", "Beetlejuice", "The Nightmare Before Christmas", "The Corpse Bride", "Frankenweenie", "Mars Attacks!", "Sweeney Todd", "Sleep Hollow", "Big Fish", "Pee-Wee's Big Adventure"
 ];
-console.log(movies);
+//console.log(movies);
 
 // Generic function for capturing the movie name from the data-attribute
 function alertMovieName() {
@@ -24,8 +24,14 @@ function renderButtons() {
         // Adding a class
         userMovie.addClass("movie");
 
+        // Adding another class so we can pause the gifs later
+        userMovie.addClass("gif");
+
         // Added a data-attribute
         userMovie.attr("data-name", movies[i]);
+
+        // Added a data-attribute
+        userMovie.attr("data-state", "still", movies[i]);
 
         // Provided the initial button text
         userMovie.text(movies[i]);
@@ -33,65 +39,88 @@ function renderButtons() {
         // Added the button to the HTML
         $("#btn-view").append(userMovie);
     }
+    console.log(userMovie);
+    console.log("data-state");
 }
 
 
+// think about on "click"
 // This function handles events where one button is clicked
 $("#add-movie").on("click", function (event) {
     event.preventDefault();
-    // This line grabs the input from the textbox
     var userMovie = $("#movie-input").val().trim();
-
+    console.log(userMovie);
+    // This line grabs the input from the textbox
     // The movie from the textbox is then added to our array
     movies.push(userMovie);
     console.log(movies);
 
     // Calling renderButtons which handles the processing of our movie array
     renderButtons();
+    appendMovie();
 });
 
 // Function for displaying the movie info
-$(document).on("click", ".movie", alertMovieName);
+// $(document).on("click", ".movie", alertMovieName);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
+appendMovie();
 
 //AJAX CALL
 
-$("button").on("click", function () {
-    var movie = $(this).attr("data-movie");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        movie + "&api_key=fuR6CscpNgfqH8BemuXHuAzAucb6xoo5&limit=5";
+function appendMovie() {
+    $("button").on("click", function () {
+        var movie = $(this).attr("data-name");
+        console.log(movie);
 
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=fuR6CscpNgfqH8BemuXHuAzAucb6xoo5&limit=5";
+        console.log(queryURL);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(response);
 
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-            var movieDiv = $("<div>");
-            var p = $("<p>").text(results[i]);
+                var results = response.data;
+                for (var i = 0; i < results.length; i++) {
+                    var movieDiv = $("<div>");
+                    var p = $("<p>").text("Rating: " + results[i].rating);
 
-            var movieImage = $("<img>");
-            movieImage.attr("src", results[i].data.images.fixed_height.url);
+                    var movieImage = $("<img>");
 
-            movieDiv.append(p);
-            movieDiv.append(movieImage);
-            $("#gifs-appear-here").prepend(movieDiv)
-        }
+                    movieImage.attr("src", results[i].images.fixed_height.url);
 
-
+                    movieDiv.append(p);
+                    movieDiv.append(movieImage);
+                    $("#gifs-appear-here").prepend(movieImage);
+                }
+            });
     });
-});
-
+}
 //EVENT LISTENER FOR BUTTON CLICKS ON THE TOPICS - FOR LATER
 
 // // Event handler for user clicking the select-movie button
-// $("#movie-input").on("click", function (event) {
+// $(".movies").on("click", function (event) {
 //     // Preventing the button from trying to submit the form
 //     event.preventDefault();
 //     // Storing the artist name
-//     var scaryMovie = $("#movie-input").val().trim();
+// });
+
+
+$(".gif").on("click", function () {
+
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
